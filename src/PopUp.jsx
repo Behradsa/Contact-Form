04398a2f9188fadb5dfe04761a299de98b0dca75
Behrad Saraiyeh <b.saraiyeh@gmail.com>
2024-06-16 00:59:10 +0330

    popup icon fixed again

--- src/PopUp.jsx
+++ src/PopUp.jsx
@@ -4,7 +4,7 @@ export default function PopUp() {
   return (
     <div className="popup scale-in-center scale-out-center">
       <div className="popup-sent-container">
-        <img src="src/assets/images/icon-success-check.svg" alt="" />
+        <img src="./assets/images/icon-success-check.svg" alt="" />
         <h3>Message Sent!</h3>
       </div>
       <p>Thanks for completing the form. We'll be in touch soon!</p>
